a34ed0e764b22150c1c7d6e013b841ab17a161a0
LuziTempest <luzinaen@gmail.com>
2024-11-24 15:06:17 +0700

    Update node.cpp

--- src/controller/src/node.cpp
+++ src/controller/src/node.cpp
@@ -15,48 +15,67 @@ class Publisher : public rclcpp::Node
 	rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr sub;
 	rclcpp::Publisher<interfaces::msg::Command>::SharedPtr pub;
 
+    float current_depth = 10.0, current_yaw = 0.0;
+    float normalize_yaw(float yaw) {
+        if (yaw > 180.0f) {
+            yaw -= 360.0f;
+        } else if (yaw < -180.0f) {
+            yaw += 360.0f;
+        }
+        return yaw;
+    }
 	void fungsi_subscribe(const sensor_msgs::msg::Joy &msg)
 	{
-	
-    float stick_x = msg.axes[0] * 250.0;  // stik kiri, sumbu X
-    float stick_y = msg.axes[1] * 250.0;  // stik kiri, sumbu Y
+        float stick_x = msg.axes[0] * 250.0;  // stik kiri, sumbu X
+        float stick_y = msg.axes[1] * 250.0;  // stik kiri, sumbu Y
 
-    float croskey_x = msg.axes[6] * 250.0;  // D-pad kiri/kanan
-    float croskey_y = msg.axes[7] * 250.0;  // D-pad atas/bawah
+        float croskey_x = msg.axes[6] * 250.0;  // D-pad kiri/kanan
+        float croskey_y = msg.axes[7] * 250.0;  // D-pad atas/bawah
 
-    float depth = (msg.axes[4] + 1) * 5;  // stik kanan, sumbu y
-    float yaw = msg.axes[3] * 180.0;  // Stick kanan, sumbu x
-    
-    float x_button = msg.buttons[2];
-    float y_button = msg.buttons[3];
-    float b_button = msg.buttons[1];
-    float a_button = msg.buttons[0];
+        float new_depth = current_depth + msg.axes[4] * 0.1;  // Mengubah nilai `depth` perlahan
+        if (new_depth > 10.0) {
+            new_depth = 10.0;  // Maksimal nilai depth
+        } else if (new_depth < 0.0) {
+            new_depth = 0.0;  // Minimal nilai depth
+        }
 
-    float x,y;
-    if (croskey_x != 0.0){
-        x = croskey_x;
-    } else {
-        x = stick_x;
-    } 
+        if (new_depth != current_depth) {
+            current_depth = new_depth;
+        }
 
-    if (croskey_y != 0.0){
-        y = croskey_y;
-    } else {
-        y = stick_y;
-    }    
+        current_yaw += msg.axes[3] * 5.0;
+        current_yaw = normalize_yaw(current_yaw);
+        
+        float x_button = msg.buttons[2];
+        float y_button = msg.buttons[3];
+        float b_button = msg.buttons[1];
+        float a_button = msg.buttons[0];
 
-    auto cmd = interfaces::msg::Command();
+        float x,y;
+        if (croskey_x != 0.0){
+            x = croskey_x;
+        } else {
+            x = stick_x;
+        } 
 
-    cmd.x_cmd = x;
-    cmd.y_cmd = y;
-    cmd.depth = depth;
-    cmd.yaw = yaw;
-    cmd.x = x_button;
-    cmd.y = y_button;
-    cmd.a = a_button;
-    cmd.b = b_button; 
+        if (croskey_y != 0.0){
+            y = croskey_y;
+        } else {
+            y = stick_y;
+        }    
 
-    pub->publish(cmd);
+        auto cmd = interfaces::msg::Command();
+
+        cmd.x_cmd = x * -1;
+        cmd.y_cmd = y;
+        cmd.depth = current_depth;
+        cmd.yaw = current_yaw;
+        cmd.x = x_button;
+        cmd.y = y_button;
+        cmd.a = a_button;
+        cmd.b = b_button; 
+
+        pub->publish(cmd);
 	}
 
 public:
